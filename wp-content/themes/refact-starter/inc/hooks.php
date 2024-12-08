6dcf3a95ed52c73121cc98cfbdbb3db9dc2eba18
Hossein Karami <hi@hosseinkarami.com>
2024-12-08 12:11:48 +0300

    feat: improve block folder handling for ACF and theme hooks

--- wp-content/themes/refact-starter/inc/hooks.php
+++ wp-content/themes/refact-starter/inc/hooks.php
@@ -13,23 +13,30 @@ function refact_starter_register_blocks() {
 	// Definitions
 	$theme_version = defined( 'THEME_VERSION' ) ? THEME_VERSION : '1.0.0';
 	$transient_key = 'refact_block_folders';
+	// Define DEVELOPMENT_MODE constant in wp-config.php to enable development mode
+	$is_development = defined( 'DEVELOPMENT_MODE' ) && DEVELOPMENT_MODE;
 
-	// Try to get the cached block folders from transients
-	$transient_data = get_transient( $transient_key );
-
-	// Check if cached data exists and matches the current theme version
-	if ( false !== $transient_data && isset( $transient_data['theme_version'] ) && $transient_data['theme_version'] === $theme_version ) {
-		$block_folders = isset( $transient_data['block_folders'] ) ? $transient_data['block_folders'] : array();
-	} else {
-		// If no cached data, retrieve block folders
+	if ( $is_development ) {
+		// Development mode: Always fetch block folders
 		$block_folders = glob( get_template_directory() . '/build/blocks/*', GLOB_ONLYDIR | GLOB_NOSORT );
+	} else {
+		// Try to get the cached block folders from transients
+		$transient_data = get_transient( $transient_key );
 
-		// Update transient data
-		$transient_data = array(
-			'theme_version' => $theme_version,
-			'block_folders' => $block_folders,
-		);
-		set_transient( $transient_key, $transient_data );
+		// Check if cached data exists and matches the current theme version
+		if ( false !== $transient_data && isset( $transient_data['theme_version'] ) && $transient_data['theme_version'] === $theme_version ) {
+			$block_folders = isset( $transient_data['block_folders'] ) ? $transient_data['block_folders'] : array();
+		} else {
+			// If no cached data, retrieve block folders
+			$block_folders = glob( get_template_directory() . '/build/blocks/*', GLOB_ONLYDIR | GLOB_NOSORT );
+
+			// Update transient data
+			$transient_data = array(
+				'theme_version' => $theme_version,
+				'block_folders' => $block_folders,
+			);
+			set_transient( $transient_key, $transient_data );
+		}
 	}
 
 	// Register each block found in the block folders
